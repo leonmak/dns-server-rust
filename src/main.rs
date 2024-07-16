800e6a9e8cb94ede325f40e47896eba87097be31
Leon Mak <leonmakansheng@gmail.com>
2024-07-16 19:44:08 +0800

    Add reading of buffer to struct and then out

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 use byteorder::{BigEndian, ByteOrder};
 use std::net::UdpSocket;
 
-trait ToBytes {
-    fn to_bytes(&self) -> Vec<u8>;
-}
-
 #[allow(dead_code)]
 struct DnsHeader {
     id: u16,      // Packet Identifier (ID)
@@ -59,19 +55,83 @@ impl DnsHeader {
         }
     }
 
-    fn set_qr(&mut self, qr: bool) {
+    fn set_is_resp(&mut self, qr: bool) {
         self.qr = qr;
+    }
+
+    fn set_id(&mut self, id: u16) {
+        self.id = id;
+    }
+    fn set_num_questions(&mut self, qdcount: u16) {
+        self.qdcount = qdcount;
+    }
+
+    fn write_bytes(&self, bytes: &mut [u8; 512]) {
+        BigEndian::write_u16(&mut bytes[0..2], self.id);
+        let flags = (self.qr as u16) << 15
+            | (self.opcode as u16) << 11
+            | (self.aa as u16) << 10
+            | (self.tc as u16) << 9
+            | (self.rd as u16) << 8
+            | (self.ra as u16) << 7
+            | (self.z as u16) << 4
+            | (self.rcode as u16);
+        BigEndian::write_u16(&mut bytes[2..4], flags);
+        BigEndian::write_u16(&mut bytes[4..6], self.qdcount);
+        BigEndian::write_u16(&mut bytes[6..8], self.ancount);
+        BigEndian::write_u16(&mut bytes[8..10], self.nscount);
+        BigEndian::write_u16(&mut bytes[10..12], self.arcount);
     }
 }
 
-fn set_17th_bit(bytes: &mut [u8; 12], value: bool) {
-    let byte_index = 2;
-    let bit_offset = 0;
+#[derive(Debug)]
+struct DnsQuestion {
+    qname: String,
+    qtype: u16,
+    qclass: u16,
+}
 
-    if value {
-        bytes[byte_index] |= 1 << (7 - bit_offset);
-    } else {
-        bytes[byte_index] &= !(1 << (7 - bit_offset));
+impl DnsQuestion {
+    fn from_bytes(buffer: &[u8], offset: &mut usize) -> Self {
+        let mut qname = String::new();
+        let i = offset;
+        loop {
+            // 1st byte is length of label
+            let len = buffer[*i] as usize;
+            *i += 1;
+            if len == 0 {
+                break;
+            }
+            qname.push_str(&String::from_utf8_lossy(&buffer[*i..*i + len]));
+            qname.push('.');
+            *i += len;
+        }
+        qname.pop(); // Remove the trailing dot
+
+        let qtype = BigEndian::read_u16(&buffer[*i..*i + 2]);
+        *i += 2;
+        let qclass = BigEndian::read_u16(&buffer[*i..*i + 2]);
+
+        DnsQuestion {
+            qname,
+            qtype,
+            qclass,
+        }
+    }
+
+    fn write_bytes(&self, buf: &mut Vec<u8>) {
+        // Write the domain name
+        for label in self.qname.split(|char| char == '.') {
+            buf.push(label.len() as u8);
+            buf.extend_from_slice(label.as_bytes());
+        }
+        buf.push(0); // Null terminator for the domain name
+
+        // Write the query type
+        buf.extend_from_slice(&self.qtype.to_be_bytes());
+
+        // Write the query class
+        buf.extend_from_slice(&self.qclass.to_be_bytes());
     }
 }
 
@@ -81,17 +141,41 @@ fn main() {
 
     // Uncomment this block to pass the first stage
     let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
-    let mut buf = [0; 12];
+    let mut buf = [0; 512];
 
     loop {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
-                println!("{:?}", buf);
-                // let mut header = DnsHeader::from_bytes(&buf);
-                // header.set_qr(true);
-                set_17th_bit(&mut buf, true);
-                println!("{:?}", buf);
+
+                let header_size = 12;
+                let mut offset = header_size; // header size, use for iterating questions
+                let mut header = DnsHeader::from_bytes(&buf[..offset]);
+
+                let mut questions = Vec::new();
+                for _ in 0..header.qdcount {
+                    let question = DnsQuestion::from_bytes(&buf, &mut offset);
+                    println!("question read: {:?}", question);
+                    questions.push(question);
+                }
+
+                // Set the response flag in the header
+                header.set_is_resp(true);
+                header.set_id(header.id);
+                header.set_num_questions(questions.len() as u16);
+                println!("num questions read: {:?}", questions.len());
+
+                // write header to first 12 bytes
+                header.write_bytes(&mut buf);
+
+                // write questions to buffer
+                let mut resp: Vec<u8> = Vec::new();
+                for question in questions {
+                    question.write_bytes(&mut resp);
+                }
+
+                assert!(resp.len() <= buf.len() - header_size);
+                buf[header_size..header_size + resp.len()].copy_from_slice(&resp);
 
                 udp_socket
                     .send_to(&buf, source)
