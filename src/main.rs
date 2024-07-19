564f1660f56faa794ed6e726000833ed4b474ed5
Leon Mak <leonmakansheng@gmail.com>
2024-07-19 10:12:52 +0800

    debug

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::net::{IpAddr, UdpSocket};
+use std::net::{IpAddr, Ipv4Addr, UdpSocket};
 
 use dns_starter_rust::runner;
 
@@ -8,7 +8,11 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
     let args: Vec<String> = env::args().collect();
-    let resolver_ip = args.get(2).unwrap().parse::<IpAddr>().unwrap();
+    let resolver_ip = args
+        .get(2)
+        .unwrap_or(&"8.8.8.8".to_owned())
+        .parse::<IpAddr>()
+        .unwrap();
     println!("{resolver_ip:?}");
 
     // Uncomment this block to pass the first stage
